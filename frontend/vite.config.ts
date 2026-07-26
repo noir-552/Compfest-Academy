9e3392cbef319bd243adf5a3120b462a37c2c038
noir-552 <muh.farid.wijdan@gmail.com>
2026-07-26 12:10:40 +0700

    ke biasa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-const apiProxyTarget = process.env.VITE_API_PROXY_TARGET ?? 'http://localhost:3001';
+const apiProxyTarget = process.env.VITE_API_PROXY_TARGET ?? 'marketplace.noiricikiwir.tech';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: apiProxyTarget,
